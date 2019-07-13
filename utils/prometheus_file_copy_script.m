5fc5dc76e69b54a56b812afb51e3cadaa2e450ee
b3rnoulli <rafal@rafalkowalski.net>
2019-07-13 11:03:17 +0800

    added extra logs

--- utils/prometheus_file_copy_script.m
+++ utils/prometheus_file_copy_script.m
@@ -2,7 +2,7 @@ clear
 clc
 
 kappas = [0.05, 0.1, 0.25, 0.5];
-betas = 0.5;
+betas = 0.6:0.1:2.5;
 
 
 for i=1:length(betas)
@@ -14,7 +14,9 @@ for i=1:length(betas)
         mkdir([root_path,'kappa/',num2str(kappa)]);
         matching_file_names = dir([root_path,'*-kappa-*',num2str(kappa),'*.mat']);
         for k = 1:length(matching_file_names)
-            movefile([matching_file_names(k).folder,'/',matching_file_names(k).name], [root_path,'kappa/',num2str(kappa),'/']);
+            file_name = [matching_file_names(k).folder,'/',matching_file_names(k).name];
+            fprintf('Copying file [%s]',file_name);
+            movefile([file_name], [root_path,'kappa/',num2str(kappa),'/']);
         end
     end
     
